a0c88f79ea76e2b2ba5bdf2164c2d6ed2be9a421
Talon <talontalon24@gmail.com>
2019-08-05 20:46:35 +0200

    outsource traverser into seperate file

--- folder_structure_navigator.py
+++ folder_structure_navigator.py
@@ -3,7 +3,6 @@
 import os
 import json
 import time
-import datetime
 import threading
 import tkinter as tk
 from tkinter import font
@@ -13,142 +12,8 @@ from tkinter.ttk import Progressbar
 # from pprint import pprint
 
 import folder_structure_backup
-
-
-def sizeof_fmt(num, suffix='B'):
-    """Human readable file size"""
-    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
-class OutOfStructureException(Exception):
-    """Raise this if beyond boundaries of Structure."""
-
-
-class JsonTraverser():
-    """Navigate through the object."""
-    def __init__(self, data):
-        self.data = data
-        # self.position = list(data.keys())
-        self.position = []
-        self.current = self.data
-        self._base = self.position[:]
-        # pprint(self.position)
-
-    def up(self):  # pylint: disable=invalid-name
-        """Go to the parent directory."""
-        if not self.position:
-            raise OutOfStructureException
-        self.position.pop()
-        current = self.data
-        for folder in self.position:
-            current = current[folder]
-        self.current = current
-        return self
-
-    def down(self, name):
-        """Go to the specified child directory."""
-        name = self.clear_name(name)
-        try:
-            self.current = self.current[name]
-            self.position.append(name)
-            return self
-        except KeyError:
-            raise FileNotFoundError
-
-    def folders(self):
-        """Get the folders in the current folder."""
-        folders = list(self.current.keys())
-        try:
-            folders.remove("__/files")
-        except ValueError:
-            pass  # Root dir
-        return folders
-
-    def files(self):
-        """Get the files in the current folder."""
-        return self.current.get("__/files", [])
-
-    def content(self):
-        """Get everything in current folder."""
-        return self.folders() + self.files()
-
-    def content_nice(self):
-        """Folder content with indictor Emoji."""
-        return (["\ud83d\udcc1 " + folder for folder in self.folders()] +
-                ["\ud83d\udcdd " + file for file in self.files()])
-
-    def current_folder_info(self):
-        """Return how many folders and how many files in the current folder."""
-        return len(self.folders()), len(self.files())
-
-    def current_path(self):
-        """Get the path of the current position."""
-        # print(self.position)
-        return os.sep.join([i.replace(os.sep, "") for i in self.position])
-
-    def subdir_info(self, name, current_directory=False):
-        """Count subfolders and files."""
-        name = self.clear_name(name)
-        folders_n = files_n = size = 0
-        if current_directory:
-            structure = self.current
-        else:
-            structure = self.current[name]
-        for _, files in self.walk(structure):
-            folders_n += 1
-            files_n += len(files)
-            try:
-                size += sum([file.get("size", 0) for file in files.values()])
-            except AttributeError:
-                pass
-        folders_n -= 1  # remove self
-        size = sizeof_fmt(size)
-        return folders_n, files_n, size
-
-    def file_info(self, name):
-        """Get information about a file."""
-
-        def timeformat(timestamp):
-            return datetime.datetime.utcfromtimestamp(
-                timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        name = self.clear_name(name)
-        try:
-            file = self.current["__/files"][name]
-        except TypeError:
-            return None, None, None, None
-        size = sizeof_fmt(file.get("size"))
-        accessed = timeformat(file.get("accessed"))
-        modified = timeformat(file.get("modified"))
-        created = timeformat(file.get("created"))
-        return (size, created, modified, accessed)
-
-    # @staticmethod
-    def walk(self, structure):
-        """Recursively walk the structure."""
-        files = structure.get("__/files", [])
-        folders = list(structure.keys())
-        if files:
-            folders.remove("__/files")
-        yield folders, files
-        if folders is None:
-            folders = []
-        for folder in folders:
-            yield from self.walk(structure[folder])
-
-    @staticmethod
-    def clear_name(name):
-        """Remove Indicator emoji if present"""
-        if name.startswith("\ud83d\udcdd") or name.startswith("\ud83d\udcc1"):
-            name = name[3:]
-        return name
-
-    def is_folder(self, name):
-        """Is given name a folder?"""
-        return self.clear_name(name) in self.folders() or name == ".."
+from traverser import JsonTraverser
+from traverser import OutOfStructureException
 
 
 class App(tk.Tk):
